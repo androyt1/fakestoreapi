30f6b54600ba64dd0449ca453b3349cff7e51da3
androyt1 <androyt1@gmail.com>
2021-04-16 12:37:49 +0100

    first commit

---
+++ src/component/Home.js
@@ -0,0 +1,142 @@
+import React,{useEffect,useState} from 'react'
+import axios from 'axios'
+import { makeStyles } from '@material-ui/core/styles';
+import {CssBaseline} from '@material-ui/core'
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import {Grid} from '@material-ui/core'
+import {Container} from '@material-ui/core'
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+	  maxWidth: 345,
+	},
+	media: {
+	  height: 0,
+	  paddingTop: '56.25%', // 16:9
+	},
+	expand: {
+	  transform: 'rotate(0deg)',
+	  marginLeft: 'auto',
+	  transition: theme.transitions.create('transform', {
+		duration: theme.transitions.duration.shortest,
+	  }),
+	},
+	expandOpen: {
+	  transform: 'rotate(180deg)',
+	},
+	avatar: {
+	  backgroundColor: red[500],
+	},
+	centerContent:{
+		margin:"0 auto"
+	}
+  }));
+  
+
+
+const Home = () => {
+	const classes = useStyles();
+	const [expanded, setExpanded] = React.useState(false);
+  
+	const handleExpandClick = () => {
+	  setExpanded(!expanded);
+	};
+
+	const[products,setProducts]=useState([])
+	const[error,setError]=useState("")
+	useEffect(async()=>{
+		try {
+				console.log("The page has loaded")
+				let res=await axios.get("https://fakestoreapi.com/products")
+				console.log("data from axios ",res.data)
+				setProducts(res.data)
+			} catch (error) {
+			setError(error.message)
+		}
+	},[])
+
+	return (
+		<div>
+			<CssBaseline>
+			<Container className={classes.centerContent}>
+			<Grid container spacing={2} className={classes.centerContent}>
+				
+				{products.map(product=>{
+				return  <Grid item xs={12} sm={6} md={4}>
+				<Card className={classes.root} key={product.id}>
+				<CardHeader
+				
+				  action={
+					<IconButton aria-label="settings">
+					  <MoreVertIcon />
+					</IconButton>
+				  }
+				  title={product.title}
+				  subheader={product.category}
+				/>
+				<CardMedia
+				  className={classes.media}
+				  image={product.image}
+				  title={product.price}
+				/>
+				<CardContent>
+				  <Typography variant="body2" color="textSecondary" component="p">
+					{product.description}
+				  </Typography>
+				</CardContent>
+				<CardActions disableSpacing>
+				  <IconButton aria-label="add to favorites">
+					<FavoriteIcon />
+				  </IconButton>
+				  <IconButton aria-label="share">
+					<ShareIcon />
+				  </IconButton>
+				  <IconButton
+					className={clsx(classes.expand, {
+					  [classes.expandOpen]: expanded,
+					})}
+					onClick={handleExpandClick}
+					aria-expanded={expanded}
+					aria-label="show more"
+				  >
+					<ExpandMoreIcon />
+				  </IconButton>
+				</CardActions>
+				<Collapse in={expanded} timeout="auto" unmountOnExit>
+				  <CardContent>
+					<Typography paragraph>Method:</Typography>
+					<Typography paragraph>
+					  Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+					  minutes.
+					</Typography>
+					
+					
+				   
+				  </CardContent>
+				</Collapse>
+			  </Card>
+			  </Grid>
+			})}
+				
+			</Grid>	
+			</Container>
+			</CssBaseline>
+		</div>
+	)
+}
+
+export default Home
